9bbf728e381684571409fc0cbc47e5aa0ec56698
jmac006 <jmac006@ucr.edu>
2017-05-20 15:34:00 -0700

    Functions for LCD and bluetooth

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,13 +1,20 @@
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
+#include <Brain.h>
+
+Brain brain(Serial);
+int attention;
 
 const int BUTTON_PIN = 5;
 const int RED_LED = 3;
 const int BLUE_LED = 2;
+const int RX = 7;
+const int TX = 8;
 
-LiquidCrystal lcd(4,5,6,12,9,13);
+//RS = PIN4, Enable = PIN5, D4-D7 = PIN6,12,9,13
+LiquidCrystal lcd(4,5,6,12,9,13); 
 
-SoftwareSerial mySerial(7, 8); // RX, TX
+SoftwareSerial mySerial(RX, TX); // RX, TX
 
 int buttonCount = 0;
 int buttonPressed = 0;
@@ -29,64 +36,67 @@ void turnOn(){
 void turnOff(){
  
 }
-void setup() {  
+void setup() {
+  attention = brain.readAttention();
 	Serial.begin(9600);
-	// If the baudrate of the HM-10 module has been updated,
-	// you may need to change 9600 by another value
-	// Once you have found the correct baudrate,
-	// you can update it using AT+BAUDx command 
-	// e.g. AT+BAUD0 for 9600 bauds
+  mySerial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
   //pinMode(BLUE_LED, OUTPUT);
   //pinMode(RED_LED, OUTPUT);
-	mySerial.begin(9600);
-  
+	
   lcd.begin(16,2);
+  analogWrite(11,255); //Powers the EEG headset
 }
 
-void loop() {  
-	
-	buttonPressed = digitalRead(BUTTON_PIN);
-	if(buttonPressed != buttonState) { //if the button state has changed
+void checkButtonPress() {
+  buttonPressed = digitalRead(BUTTON_PIN);
+  if(buttonPressed != buttonState) { //if the button state has changed
     if(buttonPressed == HIGH) {
       buttonCount++;
       //Serial.println(buttonCount);
     }
-	}
+  }
   buttonState = buttonPressed;
-  
-	if (mySerial.available()) {
-		Serial.println("bluetooth is available");
-		bluetoothRead = mySerial.read();  
-		Serial.println("Got input:");
-		if (bluetoothRead == 1)
-		{
-			// Non-zero input means "turn on LED".
-			Serial.println("Red LED On");
-			Serial.println(bluetoothRead);
+}
+
+void checkBluetooth() {
+  if (mySerial.available()) {
+    Serial.println("bluetooth is available");
+    bluetoothRead = mySerial.read();  
+    Serial.println("Got input:");
+    if (bluetoothRead == 1)
+    {
+      // Non-zero input means "turn on LED".
+      Serial.println("Red LED On");
+      Serial.println(bluetoothRead);
       analogWrite(RED_LED, 255);
-		}
-		else if (bluetoothRead == 2)
-		{
-			// Input value zero means "turn off LED".
-			Serial.println("Blue LED on");
-			Serial.println(bluetoothRead);
-			analogWrite(BLUE_LED, 255);
-		} 
-		else if (bluetoothRead == 3)
-		{
-			// Input value zero means "turn off LED".
-			Serial.println("Red LED off");
-			Serial.println(bluetoothRead);
-			digitalWrite(RED_LED, LOW);
-		}
-		else if (bluetoothRead == 4)
-		{
-			// Input value zero means "turn off LED".
-			//Serial.println("Blue LED off");
-			//Serial.println(bluetoothRead);
-			//digitalWrite(BLUE_LED, LOW);
+    }
+    else if (bluetoothRead == 2)
+    {
+      // Input value zero means "turn off LED".
+      Serial.println("Blue LED on");
+      Serial.println(bluetoothRead);
+      analogWrite(BLUE_LED, 255);
+    } 
+    else if (bluetoothRead == 3)
+    {
+      // Input value zero means "turn off LED".
+      Serial.println("Red LED off");
+      Serial.println(bluetoothRead);
+      digitalWrite(RED_LED, LOW);
+    }
+    else if (bluetoothRead == 4)
+    {
+      // Input value zero means "turn off LED".
+      //Serial.println("Blue LED off");
+      //Serial.println(bluetoothRead);
+      //digitalWrite(BLUE_LED, LOW);
       test();
-		}
-	}
+    }
+  }
+}
+
+void loop() { 
+  checkButtonPress();
+	checkBluetooth();
 }
